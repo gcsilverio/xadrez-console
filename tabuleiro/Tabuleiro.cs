03e072872502f882c3c443757ba2a7b8a2d66636
Gustavo <g.silverio@outlook.com>
2024-03-29 19:06:17 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -1,16 +1,21 @@
 ﻿namespace tabuleiro
 {
-    public class Tabuleiro
+    internal class Tabuleiro
     {
-        public int linhas {  get; set; }
-        public int colunas { get; set; }
-        private Peca[,] pecas;
+        public int Linhas {  get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
 
         public Tabuleiro(int linhas, int colunas)
         {
-            this.linhas = linhas;
-            this.colunas = colunas;
-            pecas = new Peca[linhas, colunas];
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca Peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
         }
     }
 }
